a833a8e00e71d7b23a2ba6885b9a036629f5ebad
sagar <dhakalsagar2000@gmail.com>
2022-12-27 22:05:43 +0545

    routine dev

--- redux/cart/cartSlice.js
+++ redux/cart/cartSlice.js
@@ -14,36 +14,14 @@ const cartSlice = createSlice({
         setCartItem: (state, action) => {
             state.cart = action.payload
         },
-
-        addItem: (state, action) => {
-            let tempItem = state.cart.find(item => item.product_inventory_id === action.payload.product_inventory_id)
-            if (tempItem === undefined) {
-                axios.post('/api/user/cart/', action.payload).then(res=>{
-                    if(res.data.status==200){
-                        console.log('axios post cart data', res.data.added_data);
-                        state.cart =res.data.added_data
-                        // setCartItem(res.data.added_data)
-                    }
-                    else{
-                        message.error('something went wrong on adding cart')
-                    }
-                }).catch(err=>{
-                    console.warn(err);
-                })
-                // state.cart = [...state.cart, action.payload]
-            }
-
-        },
         removeItem: (state, action) => {
             state.cart = state.cart.filter(item => item.id != action.payload)
-            console.log(state.cart);
         },
         increasecartItemvalue: (state, action) => {
             state.cart.forEach((item) => {
                 if (item.id == action.payload) {
                     item.cart_qty = item.cart_qty + 1
                     state.cart = [...state.cart]
-                    console.log(state.cart);
                 }
             })
         },
@@ -52,7 +30,6 @@ const cartSlice = createSlice({
                 if (item.id == action.payload) {
                     item.cart_qty = item.cart_qty - 1
                     state.cart = [...state.cart]
-                    console.log(state.cart);
                 }
             })
         },
